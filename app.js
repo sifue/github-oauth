bc87ff5214210dbe6fb41ebadfc832e8b5216711
Soichiro-Yoshimura <yoshimura@soichiro>
2016-02-10 20:48:07 +0900

    途中まで

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var helmet = require('helmet');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -12,6 +13,11 @@ var photos = require('./routes/photos');
 
 var app = express();
 app.use(helmet());
+
+var passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+var GitHubStrategy = require('passport-github2');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +31,40 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use(session({
+  genid: function(req) {
+    return '1234'; // use UUIDs for session IDs
+  },
+  secret: 'keyboard cat'
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new GitHubStrategy({
+  clientID: '364eec39583ec887e865',
+  clientSecret: 'd0dcdec25a48f404fc2eafa28cf9251cb8fe6df1',
+  callbackURL: 'http://localhost:8000/auth/github/callback'
+},
+  function (accessToken, refreshToken, profile, done) {
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
+    done(null, 'user');
+  }
+  ));
+
+app.get('/auth/github',
+  passport.authenticate('github', { scope: [ 'user:email' ] }));
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/users' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+
+app.use('/', passport.authenticate('github', { scope: [ 'user:email' ] }), routes);
 app.use('/users', users);
 app.use('/photos', photos);
 
